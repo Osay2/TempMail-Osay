bc2d87cb39bf1124ee3064cc4e49cf1164860264
Osay <84993769+Osay2@users.noreply.github.com>
2021-06-25 05:10:42 -0300

    Add files via upload

---
+++ tempmail.py
@@ -0,0 +1,56 @@
+#Librerias
+import time
+from guerrillamail import *
+import os
+
+#Descargas
+def downloadguerrillaemail():
+        os.system("pip install python-guerrillamail >> exodf.txt")
+
+#Variables
+banner = """╭━━━━╮╱╱╱╱╱╱╱╭━╮╭━╮╱╱╱╭╮╱╱╭━━━╮
+┃╭╮╭╮┃╱╱╱╱╱╱╱┃┃╰╯┃┃╱╱╱┃┃╱╱┃╭━╮┃
+╰╯┃┃┣┻━┳╮╭┳━━┫╭╮╭╮┣━━┳┫┃╱╱┃┃╱┃┣━━┳━━┳╮╱╭╮
+╱╱┃┃┃┃━┫┃┃┃╰╯┃┃┃┃┃┃╭╮┃┃╰┳━┫╰━╯┣━━┃╭╮┃╰━╯┃
+╱╱╰╯╰━━┻┻┻┫╭━┻╯╰╯╰┻╯╰┻┻━╯╱╰━━━┻━━┻╯╰┻━╮╭╯
+╱╱╱╱╱╱╱╱╱╱┃┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃
+╱╱╱╱╱╱╱╱╱╱╰╯╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯
+"""
+blue = "\033[1;34;40m"
+reset = "\033[0;37;40m"
+cyan = "\033[1;36;40m"
+red = "\033[1;31;40m"
+green = "\033[1;32;40m"
+yellow = "\033[1;33;40m"
+
+#Code Email
+def email():
+        session = GuerrillaMailSession()
+        print(session.get_session_state()["email_address"])
+
+#Inicio
+downloadguerrillaemail()        
+print("\n"+red+banner, blue+"By Osay\n")
+
+while True:
+        print("\n\n")
+        print(green+"\tPresione enter o cualquier letra para generar el email. Si desea salir solo escriba ¨exit¨.")
+        loop = input(cyan+">>> ")
+        if "exit" in loop:
+                print(green+"Okey, adios...")
+                os.system("exit")
+        
+        print()
+        print(green+"Generando Email...")
+        print(yellow+"")
+        email()
+        print(green+"¡Email hecho!")
+
+
+        
+
+
+
+
+
+
